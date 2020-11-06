3fc6f7b1d8ffbaf73d7756bcb8ce538790f64c52
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-06 17:56:07 -0300

    Configurando Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,34 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/eslint-recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:prettier/recommended',
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/flowtype',
+    'prettier/react',
+    'prettier/standard',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
     ],
-    globals: {
-      Atomics: 'readonly',
-      SharedArrayBuffer: 'readonly',
-    },
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 11,
-      sourceType: 'module',
-    },
-    plugins: ['@typescript-eslint'],
-    rules: {},
-  };
+  },
+};
