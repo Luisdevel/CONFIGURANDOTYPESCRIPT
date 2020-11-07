138b739c7091a9041845954a206628871f7b8c85
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-07 18:46:44 -0300

    Tipo Object (objetos em geral)

---
+++ src/04-tipo-object/index.ts
@@ -0,0 +1,17 @@
+const objectA: {
+  keyA: string;
+  keyB: string;
+  keyC?: string;
+  readonly keyE: string; // readonly o valor dessa chave não pode ser alterada
+  [key: string]: unknown;
+} = {
+  keyA: 'Valor A',
+  keyB: 'Valor B',
+  keyE: 'Valoe E',
+};
+
+objectA.keyA = 'Outro Valor';
+objectA.keyC = 'Nova chave';
+objectA.keyD = 'Nova chave';
+
+console.log(objectA);
