e27983fa3b65804e3fa6b874f73d0d3c6f454a59
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-07 00:50:03 -0300

    Tipo any

---
+++ src/02-tipo-any/index.ts
@@ -0,0 +1,8 @@
+// Utilize any somente no último caso, pois ele é um tipo em que não queremos utilizar
+function showMessage(msg: any) {
+  return msg;
+}
+
+console.log(showMessage([1, 2, 3]));
+console.log(showMessage('Hi'));
+console.log(showMessage(1));
