1cb08b64ee422fd0dba7c4cb854ad5531d160ae2
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-15 19:44:49 -0300

    Classes, métodos e atributos abstratos

---
+++ src/28-classes-metodos-atributos-abstratos/index.ts
@@ -0,0 +1,48 @@
+export abstract class Character {
+  protected abstract emoji: string;
+
+  constructor(
+    protected name: string,
+    protected attack: number,
+    protected life: number,
+  ) {}
+
+  attacked(character: Character): void {
+    this.staff();
+    character.loseLife(this.attack);
+  }
+
+  loseLife(attackForce: number): void {
+    this.life -= attackForce;
+    console.log(
+      `${this.emoji} - ${this.name} está com ${this.life} de vida...`,
+    );
+  }
+
+  abstract staff(): void;
+}
+
+export class Warrior extends Character {
+  protected emoji = '\u{1F9DD}';
+
+  staff(): void {
+    console.log(this.emoji + 'WARRIOOORRR YAAAAAAH I ATTACKEED!!!!');
+  }
+}
+export class Monster extends Character {
+  protected emoji = '\u{1F9DF}';
+
+  staff(): void {
+    console.log(this.emoji + 'MONSTER I ATTACKEEED MUHAHAHAHA!!!!');
+  }
+}
+
+const warrior = new Warrior('Warrior', 100, 1000);
+const monster = new Monster('Monster', 87, 1000);
+
+warrior.attacked(monster);
+warrior.attacked(monster);
+warrior.attacked(monster);
+monster.attacked(warrior);
+monster.attacked(warrior);
+monster.attacked(warrior);
