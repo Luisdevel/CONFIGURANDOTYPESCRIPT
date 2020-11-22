4778198ecf36c47df481af918e89f5cd0c783742
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-22 17:43:25 -0300

    votação em ts

--- src/51-votação/index.ts
+++ src/51-votação/index.ts
@@ -7,7 +7,7 @@ export class Votation {
   private _votationOptions: VotationOption[] = [];
   constructor(public details: string) {}
 
-  addVotationOptions(votationOption: VotationOption): void {
+  addVotationOption(votationOption: VotationOption): void {
     this._votationOptions.push(votationOption);
   }
 
@@ -41,9 +41,9 @@ export class VotationApp {
 }
 
 const votation1 = new Votation('Qual sua linguagem de programação favorita?');
-votation1.addVotationOptions({ option: 'Python', numberOfVotes: 0 });
-votation1.addVotationOptions({ option: 'JavaScript', numberOfVotes: 0 });
-votation1.addVotationOptions({ option: 'TypeScript', numberOfVotes: 0 });
+votation1.addVotationOption({ option: 'Python', numberOfVotes: 0 });
+votation1.addVotationOption({ option: 'JavaScript', numberOfVotes: 0 });
+votation1.addVotationOption({ option: 'TypeScript', numberOfVotes: 0 });
 votation1.vote(0);
 votation1.vote(0);
 votation1.vote(1);
@@ -53,10 +53,15 @@ votation1.vote(2);
 votation1.vote(2);
 votation1.vote(2);
 
-const votation2 = new Votation('Qual sua cor favorita?');
-votation2.addVotationOptions({ option: 'Azul', numberOfVotes: 0 });
-votation2.addVotationOptions({ option: 'Vermelho', numberOfVotes: 0 });
-votation2.addVotationOptions({ option: 'Roxo', numberOfVotes: 0 });
+const votation2 = new Votation('Qual sua fruta favorita?');
+votation2.addVotationOption({ option: 'Maçã', numberOfVotes: 0 });
+votation2.addVotationOption({ option: 'Laranja', numberOfVotes: 0 });
+votation2.addVotationOption({ option: 'Abacaxi', numberOfVotes: 0 });
+votation2.vote(0);
+votation2.vote(0);
+votation2.vote(1);
+votation2.vote(1);
+votation2.vote(1);
 votation2.vote(2);
 votation2.vote(2);
 votation2.vote(2);
@@ -64,5 +69,4 @@ votation2.vote(2);
 const votationApp = new VotationApp();
 votationApp.addVotation(votation1);
 votationApp.addVotation(votation2);
-
 votationApp.showVotations();
