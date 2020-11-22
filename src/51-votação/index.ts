8d10ca3dadff0c4b5b8bd4d7df771791f4cbe75d
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-22 17:23:32 -0300

    votation

--- src/51-votação/index.ts
+++ src/51-votação/index.ts
@@ -7,7 +7,7 @@ export class Votation {
   private _votationOptions: VotationOption[] = [];
   constructor(public details: string) {}
 
-  addVotationOption(votationOption: VotationOption): void {
+  addVotationOptions(votationOption: VotationOption): void {
     this._votationOptions.push(votationOption);
   }
 
@@ -41,9 +41,9 @@ export class VotationApp {
 }
 
 const votation1 = new Votation('Qual sua linguagem de programação favorita?');
-votation1.addVotationOption({ option: 'Python', numberOfVotes: 0 });
-votation1.addVotationOption({ option: 'JavaScript', numberOfVotes: 0 });
-votation1.addVotationOption({ option: 'TypeScript', numberOfVotes: 0 });
+votation1.addVotationOptions({ option: 'Python', numberOfVotes: 0 });
+votation1.addVotationOptions({ option: 'JavaScript', numberOfVotes: 0 });
+votation1.addVotationOptions({ option: 'TypeScript', numberOfVotes: 0 });
 votation1.vote(0);
 votation1.vote(0);
 votation1.vote(1);
@@ -54,9 +54,9 @@ votation1.vote(2);
 votation1.vote(2);
 
 const votation2 = new Votation('Qual sua cor favorita?');
-votation2.addVotationOption({ option: 'Azul', numberOfVotes: 0 });
-votation2.addVotationOption({ option: 'Vermelho', numberOfVotes: 0 });
-votation2.addVotationOption({ option: 'Roxo', numberOfVotes: 0 });
+votation2.addVotationOptions({ option: 'Azul', numberOfVotes: 0 });
+votation2.addVotationOptions({ option: 'Vermelho', numberOfVotes: 0 });
+votation2.addVotationOptions({ option: 'Roxo', numberOfVotes: 0 });
 votation2.vote(2);
 votation2.vote(2);
 votation2.vote(2);
